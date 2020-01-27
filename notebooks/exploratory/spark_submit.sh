dee8368368ec78591f765b933cc67219994f10ce
Antoine Schwartz <47638933+Antoine-Schwartz@users.noreply.github.com>
2020-01-27 17:58:03 +0100

    Update spark_submit.sh

--- notebooks/exploratory/spark_submit.sh
+++ notebooks/exploratory/spark_submit.sh
@@ -2,7 +2,4 @@
 spark-submit --deploy-mode client \
     --master yarn \
     --driver-memory 32g \
-    --conf 'spark.executorEnv.PYTHONPATH=/usr/lib/spark/python/lib/py4j-src.zip:/usr/lib/spark/python/:<CPS>{{PWD}}/pyspark.zip<CPS>{{PWD}}/py4j-src.zip' \
-    --conf spark.yarn.isPython=true \
-    --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
     notebooks/exploratory/data_refining_part_1.py
