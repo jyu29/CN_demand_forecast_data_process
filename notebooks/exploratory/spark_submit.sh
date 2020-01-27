dc6a10e28a80689c9ded0bebe4cbc4723f2d1bdf
Antoine Schwartz <47638933+Antoine-Schwartz@users.noreply.github.com>
2020-01-27 16:08:52 +0100

    Update spark_submit.sh

--- notebooks/exploratory/spark_submit.sh
+++ notebooks/exploratory/spark_submit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -x
-spark-submit --deploy-mode client --master yarn \
+spark-submit --deploy-mode client \
+    --master yarn \
+    --driver-memory 32g \
+    --executor-memory 4G \
+    --num-executors 8 \
     --conf 'spark.executorEnv.PYTHONPATH=/usr/lib/spark/python/lib/py4j-src.zip:/usr/lib/spark/python/:<CPS>{{PWD}}/pyspark.zip<CPS>{{PWD}}/py4j-src.zip' \
     --conf spark.yarn.isPython=true \
-    --conf spark.executor.memoryOverhead=8192 \
     --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
     notebooks/exploratory/data_refining_part_1.py
