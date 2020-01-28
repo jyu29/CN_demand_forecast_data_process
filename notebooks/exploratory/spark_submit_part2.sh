3c86ab2d81abc926a21d718ee087b703a8f0cd66
IdrisBenkhelil <48487135+IdrisBenkhelil@users.noreply.github.com>
2020-01-28 08:38:41 +0100

    Update spark_submit_part2.sh

--- notebooks/exploratory/spark_submit_part2.sh
+++ notebooks/exploratory/spark_submit_part2.sh
@@ -3,4 +3,8 @@ spark-submit --deploy-mode client --master yarn \
     --conf 'spark.executorEnv.PYTHONPATH=/usr/lib/spark/python/lib/py4j-src.zip:/usr/lib/spark/python/:<CPS>{{PWD}}/pyspark.zip<CPS>{{PWD}}/py4j-src.zip' \
     --conf spark.yarn.isPython=true \
     --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+    --driver-memory 19g \
+    --executor-memory 19g \
+    --driver-cores 5 \
+    --executor-memoryOverhead 2g \
     notebooks/exploratory/data_refining_part_2.py
