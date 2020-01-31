2403e6a6948b0c6027dd99c419f5772bd9bc934d
Antoine Schwartz <antoine.schwartz@decathlon.com>
2020-01-31 14:50:19 +0100

    fix

--- spark_submit_specific.sh
+++ spark_submit_specific.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 echo "Technical environment configuration file: $1"
-
-sudo pip-3.6 install PyYAML==3.13
+technical_conf_file = "conf/$1.yml"
 
 spark-submit \
     --deploy-mode client \
     --master yarn \
     --driver-memory 5g \
     --py-files src/utils.py \
-    src/data_refining_specific.py $1 conf/functional.yml
+    src/data_refining_specific.py $technical_conf_file conf/functional.yml
