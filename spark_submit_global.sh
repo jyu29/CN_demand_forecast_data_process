30c0b1781b8a15fcfb2f5fbc1afa0c33cb477f32
Antoine Schwartz <antoine.schwartz@decathlon.com>
2020-01-31 15:01:32 +0100

    fix

--- spark_submit_global.sh
+++ spark_submit_global.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 echo "Technical environment configuration file: $1"
-technical_conf_file = "conf/$1.yml"
+
+technical_conf_file="conf/$1.yml"
 echo "The technical configuration file is $technical_conf_file"
 
 spark-submit \
