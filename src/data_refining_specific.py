fa7780a97d9cc4cf30f5ba932ebbbad42148ab7b
Gabs619 <gabriel.abdoudingaodoumbaye.partner@decathlon.com>
2020-02-25 16:23:29 +0100

    feat: Back things to normal for data-refining global and specific

--- src/data_refining_specific.py
+++ src/data_refining_specific.py
@@ -254,8 +254,6 @@ print('filter_type: ', filter_type)
 print('filter_val: ', filter_val)
 print('only_last: ', only_last)
 
-assert 1 == 2, "Crash test"
-
 actual_sales, active_sales, model_info = read_clean_data()
 
 actual_sales, active_sales = filter_data_scope(actual_sales, active_sales, model_info)
