dff5dd79f92849f82439cf25b216d2bfd45442b3
Gabs619 <gabriel.abdoudingaodoumbaye.partner@decathlon.com>
2020-03-06 10:46:09 +0100

    feat: Improvements for data refining specific

--- src/data_refining_specific.py
+++ src/data_refining_specific.py
@@ -52,21 +52,13 @@ def filter_data_scope(actual_sales, active_sales, model_info):
 
     if scope != 'full_scope':
         actual_sales = actual_sales \
-            .join(model_info.select(model_info['model'], model_info[filter_type]),
-                  on='model',
-                  how='left')
-
-        actual_sales = actual_sales \
-            .filter(actual_sales[filter_type].isin(filter_val)) \
+            .join(model_info.select(['model'] + [filter_type]), on='model', how='left') \
+            .filter(F.col(filter_type).isin(filter_val)) \
             .drop(filter_type)
 
         active_sales = active_sales \
-            .join(model_info.select(model_info['model'], model_info[filter_type]),
-                  on='model',
-                  how='left')
-
-        active_sales = active_sales \
-            .filter(active_sales[filter_type].isin(filter_val)) \
+            .join(model_info.select(['model'] + [filter_type]), on='model', how='left') \
+            .filter(F.col(filter_type).isin(filter_val)) \
             .drop(filter_type)
 
         active_sales.persist(StorageLevel.MEMORY_ONLY)
@@ -80,82 +72,95 @@ def filter_data_scope(actual_sales, active_sales, model_info):
 ## Reconstruction function
 def reconstruct_history(train_data_cutoff, actual_sales, model_info,
                         cluster_keys=['product_nature', 'family'], min_ts_len=160):
-    last_week = train_data_cutoff.agg(F.max('week_id').alias('last_week'))
-    model_to_keep = train_data_cutoff.groupBy('model').agg(F.max('week_id').alias('last_active_week'))
-    model_to_keep = model_to_keep.join(last_week,
-                                       on=last_week.last_week == model_to_keep.last_active_week,
-                                       how='inner').select(model_to_keep.model)
-
-    train_data_cutoff = train_data_cutoff.join(model_to_keep, on='model', how='inner')
-
-    df_date = actual_sales.select(['week_id', 'date']).drop_duplicates()
-    y_not_null = train_data_cutoff.filter(train_data_cutoff.y.isNotNull())
-
+    # Create a complete TS dataframe
     max_week = train_data_cutoff.select(F.max('week_id')).collect()[0][0]
-    min_week = train_data_cutoff.select(F.min('week_id')).collect()[0][0]
 
-    list_weeks = ut.find_weeks(min_week, max_week)
-    list_weeks = spark.createDataFrame(list_weeks, IntegerType()).selectExpr('value as week_id')
-    list_models = train_data_cutoff.select(train_data_cutoff.model).distinct()
+    all_model = train_data_cutoff.select('model').orderBy('model').drop_duplicates()
+    all_week = actual_sales \
+        .filter(actual_sales.week_id <= max_week) \
+        .select('week_id') \
+        .orderBy('week_id') \
+        .drop_duplicates()
 
-    full = list_weeks.crossJoin(list_models)
-    full_actives_sales = full.join(train_data_cutoff, ['week_id', 'model'], how='left')
+    complete_ts = all_model.crossJoin(all_week)
 
-    # Add cluster infos
-    mdl_inf = model_info.select(['model'] + cluster_keys)
+    # Add corresponding date
+    complete_ts = complete_ts \
+        .join(actual_sales.select(['week_id', 'date']).drop_duplicates(),
+              on=['week_id'],
+              how='inner')
 
-    complete_ts = full_actives_sales.join(mdl_inf, on='model', how='left').drop('date')
-    complete_ts = complete_ts.join(df_date, on='week_id', how='inner')
+    # Add cluster_keys info from model_info
+    # /!\ drop_na because in very rare cases, the models are too old or too recent
+    #     and do not have descriptions in d_sku
+
+    cluster_info = model_info.select(['model'] + cluster_keys)
+
+    complete_ts = complete_ts \
+        .join(cluster_info, on='model', how="left") \
+        .dropna(subset=cluster_keys)
+
+    # Add active sales from train_data_cutoff
+    complete_ts = complete_ts.join(train_data_cutoff,
+                                   on=['model', 'week_id', 'date'],
+                                   how="left")
 
     # Calculate the average sales per cluster and week from actual_sales
-    all_sales = actual_sales.join(mdl_inf, 'model', how='left')
-    all_sales = all_sales.dropna()
-    join_key = ['week_id', 'date'] + cluster_keys
-    all_sales = all_sales.groupBy(join_key).agg(F.mean('y').alias('mean_cluster_y'))
+    all_sales = actual_sales \
+        .join(cluster_info, on='model', how='left') \
+        .dropna() \
+        .groupBy(['week_id', 'date'] + cluster_keys) \
+        .agg(F.mean('y').alias('mean_cluster_y'))
 
     # Add it to complete_ts
     complete_ts = complete_ts.join(all_sales,
                                    on=['week_id', 'date', 'product_nature', 'family'],
                                    how='left')
 
-    # Scale factor
-    complete_ts = complete_ts.withColumn('row_scale_factor',
-                                         complete_ts.y / complete_ts.mean_cluster_y)
+    # Compute the scale factor by row
+    complete_ts = complete_ts \
+        .withColumn('row_scale_factor', complete_ts.y / complete_ts.mean_cluster_y)
 
+    # Compute the scale factor by model
     model_scale_factor = complete_ts \
         .groupBy('model') \
         .agg(F.mean('row_scale_factor').alias('model_scale_factor'))
 
     complete_ts = complete_ts.join(model_scale_factor, on='model', how='left')
 
-    assert complete_ts.where(complete_ts.model_scale_factor.isNull()).count() == 0
+    # have each model a scale factor?
+    assert complete_ts.filter(complete_ts.model_scale_factor.isNull()).count() == 0
 
-    # Compute fake Y
+    # Compute a fake Y by row (if unknow fill by 0)
     complete_ts = complete_ts \
         .withColumn('fake_y',
                     (complete_ts.mean_cluster_y * complete_ts.model_scale_factor).cast('int'))
     complete_ts = complete_ts.fillna(0, subset=['fake_y'])
 
-    start_end = y_not_null \
+    # Calculate real age & total length of each TS
+    # And estimate the implementation period: while fake y > y
+    ts_start_end_date = complete_ts \
+        .filter(complete_ts.y.isNotNull()) \
         .groupBy('model') \
         .agg(F.min('date').alias('start_date'), F.max('date').alias('end_date'))
 
-    complete_ts = complete_ts.join(start_end, 'model', how='left')
+    complete_ts = complete_ts.join(ts_start_end_date, on='model', how='left')
 
     complete_ts = complete_ts \
-        .withColumn('age', (F.datediff(F.col('date'), F.col('start_date'))) / (7) + 1) \
-        .withColumn('length', (F.datediff(F.col('end_date'), F.col('date'))) / (7) + 1) \
+        .withColumn('age', (F.datediff(F.col('date'), F.col('start_date')) / 7) + 1) \
+        .withColumn('length', (F.datediff(F.col('end_date'), F.col('date')) / 7) + 1) \
         .withColumn('is_y_sup', F.when(complete_ts.y.isNull(), 'false') \
                     .when(complete_ts.y > complete_ts.fake_y, 'true') \
                     .otherwise('false'))
 
     end_impl_period = complete_ts \
         .filter(complete_ts.is_y_sup == True) \
-        .select(['model', 'age']).groupBy('model') \
+        .groupBy('model') \
         .agg(F.min('age').alias('end_impl_period'))
 
-    complete_ts = complete_ts.join(end_impl_period, on=['model'], how='left')
+    complete_ts = complete_ts.join(end_impl_period, on='model', how='left')
 
+    # Update y from "min_ts_len" weeks ago to the end of the implementation period
     complete_ts = complete_ts \
         .withColumn('y',
                     F.when(((complete_ts.age <= 0) & (complete_ts.length <= min_ts_len)) | \
@@ -165,7 +170,7 @@ def reconstruct_history(train_data_cutoff, actual_sales, model_info,
 
     complete_ts = complete_ts \
         .select(['week_id', 'date', 'model', 'y']) \
-        .dropna(subset=('week_id', 'date', 'model', 'y')) \
+        .dropna() \
         .orderBy(['week_id', 'model'])
 
     return complete_ts
@@ -175,55 +180,57 @@ def reconstruct_history(train_data_cutoff, actual_sales, model_info,
 
 # Generate training data used to forecast validation & test cutoffs
 def generate_cutoff_train_data(actual_sales, active_sales, model_info, only_last):
-    current_cutoff = ut.get_next_week_id(actual_sales.select(F.max('week_id')).collect()[0][0])
+    current_cutoff = ut.get_current_week_id()
 
     if only_last:
         l_cutoff_week_id = [current_cutoff]
     else:
-        cutoff_week_test = active_sales \
+        cutoff_week_id = active_sales \
             .filter(active_sales.week_id >= first_test_cutoff) \
-            .select(active_sales.week_id) \
+            .select('week_id') \
             .drop_duplicates() \
             .orderBy('week_id')
 
-        nRow = spark.createDataFrame([[current_cutoff]])
-        iterate_week = cutoff_week_test.union(nRow)
-        l_cutoff_week_id = [row.week_id for row in iterate_week.collect()]
+        l_cutoff_week_id = [row['week_id'] for row in cutoff_week_id.collect()] + [current_cutoff]
 
     # loop generate cutoffs
-    for cutoff_week_id in sorted(l_cutoff_week_id):
+    for cutoff_week_id in l_cutoff_week_id:
         t0 = time.time()
+
         print('Generating train data for cutoff', str(cutoff_week_id))
 
         train_data_cutoff = active_sales.filter(active_sales.week_id < cutoff_week_id)
 
-        model_sold = train_data_cutoff.select(['model', 'y']) \
+        # Models sold at least once before the cutoff
+        model_sold = train_data_cutoff \
             .groupBy('model') \
-            .agg(F.sum(train_data_cutoff.y).alias('qty_sold')) \
-            .orderBy('model')
+            .agg(F.sum('y').alias('qty_sold')) \
+            .filter(F.col('qty_sold') > 0) \
+            .select('model')
 
-        model_sold = model_sold.filter(model_sold.qty_sold > 0).select('model').orderBy('model')
-
+        # Active the last week before the cutoff
         last_week = train_data_cutoff.agg(F.max('week_id').alias('last_week'))
 
-        model_active = train_data_cutoff.groupBy('model').agg(F.max('week_id').alias('last_active_week'))
+        model_active = train_data_cutoff \
+            .groupBy('model') \
+            .agg(F.max('week_id').alias('last_active_week'))
 
         model_active = model_active \
             .join(last_week,
                   on=last_week.last_week == model_active.last_active_week,
                   how='inner') \
-            .select(model_active.model)
+            .select('model')
 
+        # Keep only sold & active models
         model_to_keep = model_active.join(model_sold, 'model', 'inner')
-
         train_data_cutoff = train_data_cutoff.join(model_to_keep, on='model', how='inner')
 
         # Reconstruct a fake history
         train_data_cutoff = reconstruct_history(train_data_cutoff, actual_sales, model_info)
 
-        path_cutoff = '{}/train_data_cutoff/train_data_cutoff_{}'.format(scope, str(cutoff_week_id))
+        cutoff_path = '{}/train_data_cutoff/train_data_cutoff_{}'.format(scope, str(cutoff_week_id))
 
-        ut.write_parquet_s3(train_data_cutoff, s3_path_refine_specific, path_cutoff)
+        ut.write_parquet_s3(train_data_cutoff, s3_path_refine_specific, cutoff_path)
 
         t1 = time.time()
         total = t1 - t0
@@ -259,6 +266,10 @@ print("Count model_info = {}".format(model_info_count))
 
 
 actual_sales, active_sales = filter_data_scope(actual_sales, active_sales, model_info)
+
+assert actual_sales.count() > 0
+assert active_sales.count() > 0
+assert model_info.count() > 0
 
 generate_cutoff_train_data(actual_sales, active_sales, model_info, only_last=only_last)
 
