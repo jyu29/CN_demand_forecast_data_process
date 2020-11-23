5e30f6861382cdea50e1e6d1ca10dec911ec0456
Antoine-schwartz <schwartz.antoine.pro@gmail.fr>
2020-11-23 13:50:07 +0000

    fix: missing backslashes

--- main_spark.sh
+++ main_spark.sh
@@ -12,17 +12,17 @@ sudo pip-3.6 install -r requirements.txt
 spark-submit \
     --deploy-mode client \
     --master yarn \
-    --conf spark.maximizeResourceAllocation=false
-    --conf spark.dynamicAllocation.enabled=false
-    --conf spark.executors.cores=5
-    --conf spark.executor.memory=3g 
-    --conf spark.executor.memoryOverhead=1g 
-    --conf spark.executor.instances=8
-    --conf spark.default.parallelism=80
-    --conf spark.sql.shuffle.partitions=80
-    --conf spark.yarn.am.cores=5
-    --conf spark.yarn.am.memory=3g 
-    --conf spark.yarn.am.memoryOverhead=1g 
+    --conf spark.maximizeResourceAllocation=false \
+    --conf spark.dynamicAllocation.enabled=false \
+    --conf spark.executors.cores=5 \
+    --conf spark.executor.memory=3g \
+    --conf spark.executor.memoryOverhead=1g \
+    --conf spark.executor.instances=8 \
+    --conf spark.default.parallelism=80 \
+    --conf spark.sql.shuffle.partitions=80 \
+    --conf spark.yarn.am.cores=5 \
+    --conf spark.yarn.am.memory=3g \
+    --conf spark.yarn.am.memoryOverhead=1g \
 	--py-files src/utils.py \
 	./src/data_refining_global.py $technical_conf_file
 
