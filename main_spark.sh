3cd06ec9f64216e61ff6c42cb5b01e0de9a35f11
BenjaminDeffarges <benjamin.deffarges@decathlon.com>
2021-10-21 15:26:23 +0200

    tools.zip

--- main_spark.sh
+++ main_spark.sh
@@ -8,6 +8,7 @@ technical_conf_file="./config/$technical_env.yml"
 echo "Technical configuration file: $technical_conf_file"
 
 sudo pip3 install -r requirements.txt
+zip -r tools.zip ./src/tools
 
 echo "Wait 60s until the initialization of metastore is done"
 # Wait 60s until the initialization of metastore is done
@@ -18,7 +19,7 @@ echo "Spark submit :"
 spark-submit \
     --deploy-mode client \
     --master yarn \
-	  --py-files src/tools/utils.py,src/tools/date_tools.py,src/tools/get_config.py,src/tools/parse_config.py \
+	  --py-files tools.zip \
 	  ./src/refining_global/main_data_refining_global.py -c $technical_conf_file
 
 echo $? > code_status
