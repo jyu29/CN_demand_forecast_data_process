8de64cb37e1e550ff04d03460319de6da4016b49
Hank Tsai <48052076+HankTsai@users.noreply.github.com>
2021-11-17 13:09:55 +0800

    debug from whitelist requirement: add variable in spark_submit_refining_global.sh

--- spark_submit_refining_global.sh
+++ spark_submit_refining_global.sh
@@ -5,7 +5,9 @@ technical_env=${1:-dev}
 echo "Technical environment: $technical_env"
 
 technical_conf_file="./config/$technical_env.yml"
+white_list_file = "./config/quicktest_whitelist.yml"
 echo "Technical configuration file: $technical_conf_file"
+echo "WhiteList file: $white_list_file"
 
 sudo pip3 install -r requirements.txt
 zip -x ./src/refining_specific* -x ./src/refining_global* -r tools.zip ./src/
@@ -18,7 +20,7 @@ spark-submit \
     --deploy-mode client \
     --master yarn \
     --py-files tools.zip \
-    ./src/refining_global/main_data_refining_global.py -c $technical_conf_file
+    ./src/refining_global/main_data_refining_global.py -c $technical_conf_file -w $white_list_file
 
 echo $? > code_status
 my_exit_code=$(cat code_status)
