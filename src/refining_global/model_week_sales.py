5a7f7e93db48dface33bd69ba04644f5848dc954
Hank Tsai <48052076+HankTsai@users.noreply.github.com>
2021-12-30 16:21:38 +0800

    re-add channel for online sales to test "sale per groth." (debug 1)

--- src/refining_global/model_week_sales.py
+++ src/refining_global/model_week_sales.py
@@ -39,7 +39,7 @@ def get_offline_sales(tdt, day, week, sku, but, cex, sapb):
     return offline_sales
 
 
-def get_online_sales(dyd, day, week, sku, but, gdc, cex, sapb, channel):
+def get_online_sales(dyd, day, week, sku, but, gdc, cex, sapb):
     """
     Get online sales from delivery data
     Filters:
@@ -59,9 +59,6 @@ def get_online_sales(dyd, day, week, sku, but, gdc, cex, sapb, channel):
         .join(F.broadcast(but),
               on=dyd['but_idr_business_unit_stock_origin'] == but['but_idr_business_unit'],
               how='inner') \
-        .join(F.broadcast(channel),
-              on=dyd['the_transaction_id'] == channel['channel_key'],
-              how='left') \
         .join(F.broadcast(gdc),
               on=but['but_code_international'] == F.concat(gdc['ean_1'], gdc['ean_2'], gdc['ean_3']),
               how='inner') \
@@ -80,9 +77,8 @@ def get_online_sales(dyd, day, week, sku, but, gdc, cex, sapb, channel):
                 dyd['f_qty_item'],
                 dyd['f_tdt_pri_regular_sales_unit'].alias('f_pri_regular_sales_unit'),
                 dyd['f_to_tax_in'],
-                cex['exchange_rate'],
-                channel['channel_name'].alias('channel'))\
-        .cache()
+                cex['exchange_rate'])\
+        .withColumn("channel", F.lit('online'))
     return online_sales
 
 
@@ -106,12 +102,12 @@ def union_sales(offline_sales, online_sales, current_week):
     return model_week_sales
 
 
-def get_model_week_sales(tdt, dyd, day, week, sku, but, cex, sapb, gdc, current_week, channel):
+def get_model_week_sales(tdt, dyd, day, week, sku, but, cex, sapb, gdc, current_week):
     # Get offline sales
     offline_sales = get_offline_sales(tdt, day, week, sku, but, cex, sapb)
 
     # Get online sales
-    online_sales = get_online_sales(dyd, day, week, sku, but, gdc, cex, sapb, channel)
+    online_sales = get_online_sales(dyd, day, week, sku, but, gdc, cex, sapb)
 
     # Create model week sales
     model_week_sales = union_sales(offline_sales, online_sales, current_week)
