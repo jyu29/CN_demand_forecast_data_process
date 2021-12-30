bf8f82e7a1b96dea583089b00fa32798e3d40a26
Hank Tsai <48052076+HankTsai@users.noreply.github.com>
2021-12-30 15:50:37 +0800

    re-add channel for online sales to test "sale per groth."

--- src/refining_global/main_data_refining_global.py
+++ src/refining_global/main_data_refining_global.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     zep = ut.spark_read_parquet_s3(spark, bucket_clean, path_clean_datalake + 'ecc_zaa_extplan/')
 
     # Apply global filters
-    # dyd = gf.filter_dyd(dyd)
+    dyd = gf.filter_dyd(dyd)
     cex = gf.filter_current_exchange(cex)
     sku = gf.filter_sku(sku)
     sku_h = gf.filter_sku(sku_h)
@@ -59,10 +59,10 @@ if __name__ == '__main__':
     week = gf.filter_week(week, params.first_historical_week, current_week)
     sapb = gf.filter_sapb(sapb, params.list_purch_org)
     gdw = gf.filter_gdw(gdw)
-    # channel = gf.filter_channel(but)
+    channel = gf.filter_channel(but)
 
     # Create model_week_sales
-    model_week_sales = sales.get_model_week_sales(tdt, dyd, day, week, sku, but, cex, sapb, gdc, current_week)
+    model_week_sales = sales.get_model_week_sales(tdt, dyd, day, week, sku, but, cex, sapb, gdc, current_week, channel)
     model_week_sales.persist(StorageLevel.MEMORY_ONLY)
     print('====> counting(cache) [model_week_sales] took ')
     start = time.time()
