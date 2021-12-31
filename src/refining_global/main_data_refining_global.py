6ed2e512d5ccf2178bfb9985ac7e194fb848d436
Hank Tsai <48052076+HankTsai@users.noreply.github.com>
2021-12-31 10:32:04 +0800

    re-add channel for online sales to test "sale per growth." (debug 8)

--- src/refining_global/main_data_refining_global.py
+++ src/refining_global/main_data_refining_global.py
@@ -51,14 +51,14 @@ if __name__ == '__main__':
     zep = ut.spark_read_parquet_s3(spark, bucket_clean, path_clean_datalake + 'ecc_zaa_extplan/')
 
     # Apply global filters
-    # cex = gf.filter_current_exchange(cex)
+    cex = gf.filter_current_exchange(cex)
     sku = gf.filter_sku(sku)
     sku_h = gf.filter_sku(sku_h)
     day = gf.filter_day(day, params.first_historical_week, current_week)
     week = gf.filter_week(week, params.first_historical_week, current_week)
     sapb = gf.filter_sapb(sapb, params.list_purch_org)
     gdw = gf.filter_gdw(gdw)
-    channel = gf.filter_channel(but)
+    # channel = gf.filter_channel(but)
     # dyd = gf.filter_dyd(dyd)
 
     # Create model_week_sales
